c899e8d5b8962d9216b5fd8b9173df005105e9c3
Vishal Bagal <bagal9464@gmail.com>
2025-06-11 23:17:55 +0530

    Update Python Basic code.py

--- Basic Python/Python Basic code.py
+++ Basic Python/Python Basic code.py
@@ -1,5 +1,4 @@
-# Introduction
-# Day 1 - 30DaysOfPython Challenge
+
 
 print(3 + 2)   # addition(+)
 print(3 - 2)   # subtraction(-)
@@ -22,3 +21,34 @@ print(type((9.8, 3.14, 2.7)))    # Tuple
 print(type(3 == 3))              # Bool
 print(type(3 >= 3))              # Bool
 
+
+# Variables in Python
+
+first_name = 'Vishal'
+last_name = 'Bagal'
+country = 'IND'
+city = 'Latur'
+age = 21
+is_married = False
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Vishal', 'Bagal', 'IND', 21, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
